8bd0f78db30e724b5e66022ebfcef2802cac5daf
kodemeister <kodemeister@outlook.com>
2022-02-27 04:54:16 +0600

    Improve bootstrapping of packer.nvim

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,10 +1,11 @@
 -- Automatically install packer.nvim on the first run.
 local data_dir = vim.fn.stdpath('data')
 local packer_dir = data_dir .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(packer_dir)) ~= 0 then
+local packer_bootstrap = vim.fn.empty(vim.fn.glob(packer_dir)) ~= 0
+if packer_bootstrap then
   local packer_url = 'https://github.com/wbthomason/packer.nvim'
-  vim.fn.system({'git', 'clone', packer_url, packer_dir})
-  vim.cmd('packadd packer.nvim')
+  vim.fn.system({'git', 'clone', '--depth', '1', packer_url, packer_dir})
+  vim.o.runtimepath = data_dir .. '/site/pack/*/start/*,' .. vim.o.runtimepath
 end
 
 -- Declare the required plugins.
@@ -79,4 +80,8 @@ require('packer').startup(function(use)
   })
 
   use('nelstrom/vim-visual-star-search')
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
