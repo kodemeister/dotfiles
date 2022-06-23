57f5da31c6ffc8222d03316f502f04580022e05f
kodemeister <kodemeister@outlook.com>
2022-06-23 23:01:51 +0600

    Move configuration from fish_variables to config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,18 @@ set -x PAGER less
 # Add user-wide bin directories to $fish_user_paths if they exist.
 fish_add_path -g ~/.cabal/bin ~/.ghcup/bin ~/.local/bin ~/bin
 
+# Suppress the greeting message.
+set fish_greeting
+
+# Switch to Vi mode.
+fish_vi_key_bindings
+
+# Define cursor shapes for Vi mode.
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
+
 # Define useful key bindings for Vi mode.
 bind -M insert \cy accept-autosuggestion
 bind -M insert \cp up-or-search
