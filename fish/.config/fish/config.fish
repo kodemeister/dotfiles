e27767afeb43f06a23f8316218b010b7d7c63fb7
kodemeister <kodemeister@outlook.com>
2022-06-23 15:36:18 +0600

    Remove check for interactive session from fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,12 @@
-if status is-interactive
-  # Setup editor environment variables.
-  set -x EDITOR nvim
-  set -x VISUAL nvim
-  set -x PAGER less
+# Setup editor environment variables.
+set -x EDITOR nvim
+set -x VISUAL nvim
+set -x PAGER less
 
-  # Add user-wide bin directories to $fish_user_paths if they exist.
-  fish_add_path -g ~/.cabal/bin ~/.ghcup/bin ~/.local/bin ~/bin
+# Add user-wide bin directories to $fish_user_paths if they exist.
+fish_add_path -g ~/.cabal/bin ~/.ghcup/bin ~/.local/bin ~/bin
 
-  # Define useful key bindings for Vi mode.
-  bind -M insert \cy accept-autosuggestion
-  bind -M insert \cp up-or-search
-  bind -M insert \cn down-or-search
-end
+# Define useful key bindings for Vi mode.
+bind -M insert \cy accept-autosuggestion
+bind -M insert \cp up-or-search
+bind -M insert \cn down-or-search
