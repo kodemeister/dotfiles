6634ecd7309f69bae13e283464185223348a08f4
kodemeister <kodemeister@outlook.com>
2022-06-23 15:46:51 +0600

    Add fundle plugin manager

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,3 +10,11 @@ fish_add_path -g ~/.cabal/bin ~/.ghcup/bin ~/.local/bin ~/bin
 bind -M insert \cy accept-autosuggestion
 bind -M insert \cp up-or-search
 bind -M insert \cn down-or-search
+
+# Automatically install fundle on the first run.
+if not functions -q fundle
+  eval (curl -sfL https://git.io/fundle-install)
+end
+
+# Declare the required plugins.
+fundle init
