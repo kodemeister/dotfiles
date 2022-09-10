49113ee98260062a992a258125fbc54d8e76854a
kodemeister <kodemeister@outlook.com>
2022-09-10 06:02:32 +0600

    Move setup of XDG environment variables to xdg_variables.conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,19 +36,3 @@ fundle plugin jorgebucaran/autopair.fish
 fundle plugin patrickf1/colored_man_pages.fish
 fundle plugin IlanCosman/tide@v5
 fundle init
-
-# Define XDG variables.
-set -q XDG_CONFIG_HOME || set -g XDG_CONFIG_HOME ~/.config
-set -q XDG_CACHE_HOME || set -g XDG_CACHE_HOME ~/.cache
-set -q XDG_DATA_HOME || set -g XDG_DATA_HOME ~/.local/share
-set -q XDG_STATE_HOME || set -g XDG_STATE_HOME ~/.local/state
-
-# Force GHCup to use XDG style directories.
-set -x GHCUP_USE_XDG_DIRS 1
-
-# Force Cabal to use XDG style directories.
-set -x CABAL_CONFIG $XDG_CONFIG_HOME/cabal/config
-set -x CABAL_DIR $XDG_DATA_HOME/cabal
-
-# Force Stack to use XDG style directories.
-set -x STACK_ROOT $XDG_DATA_HOME/stack
