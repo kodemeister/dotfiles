86e445def94c9bfaed9ecce625e3b507ee4fdb3c
Arthur Kasimov <kodemeister@outlook.com>
2025-01-26 06:44:05 +0600

    Add lualine.nvim plugin

---
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      -- Set lualine theme to match current Neovim color scheme.
+      theme = "github_dark"
+    },
+    sections = {
+      -- Display the number of pending plugin updates from lazy.nvim.
+      lualine_x = {
+        {
+          require("lazy.status").updates,
+          cond = require("lazy.status").has_updates
+        }
+      }
+    }
+  }
+}
