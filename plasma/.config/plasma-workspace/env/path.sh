7a5ab9a1eafc4a99e67b602dcac85a38d282c905
Arthur Kasimov <kodemeister@outlook.com>
2025-02-26 22:50:17 +0600

    Add ~/bin and ~/.local/bin to $PATH in Plasma session

--- plasma/.config/plasma-workspace/env/path.sh
+++ plasma/.config/plasma-workspace/env/path.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # Add user directories to $PATH if they exist.
+if [[ -d "$HOME/bin" ]]; then
+  export PATH="$HOME/bin:$PATH"
+fi
+if [[ -d "$HOME/.local/bin" ]]; then
+  export PATH="$HOME/.local/bin:$PATH"
+fi
 if [[ -d "$HOME/.ghcup/bin" ]]; then
   export PATH="$HOME/.ghcup/bin:$PATH"
 fi
